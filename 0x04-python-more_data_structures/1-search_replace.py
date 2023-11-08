6c7beb10082bf9948f272934f8ec16dcbaaa558d
muyenzi10 <muyenzija10@gmail.com>
2023-11-08 06:49:39 -0800

    updated

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new = []
+    for data in my_list:
+        if data == search:
+          new.append(replace)
+        else:
+            new.append(data) 
+    return new
