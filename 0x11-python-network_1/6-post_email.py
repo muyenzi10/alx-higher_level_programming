361ed7991994e5be17ba73333ae5bd734144b9ef
muyenzi10 <muyenzija10@gmail.com>
2024-03-01 11:57:58 -0800

    updated

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Script that takes
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    req = requests.post(url, data=value)
+
+    print(req.text)
